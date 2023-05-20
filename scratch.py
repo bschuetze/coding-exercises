62cf5591b2de5f279ca96f38be87a9bc02af0c54
Brent Schuetze <brent.schuetze@anu.edu.au>
2023-05-20 16:42:36 +1000

    Update scratch

--- scratch.py
+++ scratch.py
@@ -1,19 +1,9 @@
-class Solution:
-    def fizzBuzz(self, n: int) -> list[str]:
-        result = []
-        if n > 0:
-            for i in range(1, n + 1):
-
-                fizz = i % 3 == 0
-                buzz = i % 5 == 0
-                if not fizz and not buzz:
-                    result.append(str(i))
-                else:
-                    val = ""
-                    if fizz:
-                        val = "Fizz"
-                    if buzz:
-                        val += "Buzz"
-                    result.append(val)
-                
-            return result
+for i in range(1, 101):
+    if i % 5 == 0 and i % 3 == 0:
+        print("SiteHost")
+    elif i % 3 == 0:
+        print("Site")
+    elif i % 5 == 0:
+        print("Host")
+    else:
+        print(str(i))
